f0f0904cf6453db1f32e227c40938990b63a5a98
Michael Gingras <mcg79@cornell.edu>
2023-03-29 21:03:00 -0600

    changing try catches

--- pages/api/qstash.ts
+++ pages/api/qstash.ts
@@ -5,30 +5,35 @@ import { TransactionReceiptNotFoundError, TransactionReceipt } from "viem";
 import { postData } from "@/lib/fetch";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  let receipt;
   try {
-    const receipt = (await publicClient.getTransactionReceipt({
+    receipt = (await publicClient.getTransactionReceipt({
       hash: req.body.hash as `0x${string}`,
     })) as TransactionReceipt;
 
     console.log(receipt);
-
-    const fullEndpoint = `${process.env.API_HOST}${req.body.endpoint}`;
-    console.log(fullEndpoint);
-    await postData(fullEndpoint, {
-      receipt: {
-        status: receipt.status,
-        transactionHash: receipt.transactionHash,
-      },
-    });
-
-    res.status(200).end();
   } catch (err) {
     if (err instanceof TransactionReceiptNotFoundError) {
       // 202 seems more appropriate but q-stash only retries if it gets a non 2xx response code
       console.log("error, probably failed transaction, pending result...");
       res.status(500).end();
     }
+  }
+
+  try {
+    const fullEndpoint = `${process.env.API_HOST}${req.body.endpoint}`;
+    if (receipt) {
+      await postData(fullEndpoint, {
+        receipt: {
+          status: receipt.status,
+          transactionHash: receipt.transactionHash,
+        },
+      });
+    }
+    res.status(200).end();
+  } catch (err) {
+    console.log("Error: \n" + "```" + JSON.stringify(err) + "```");
+    res.status(500).end();
   }
 }
 
