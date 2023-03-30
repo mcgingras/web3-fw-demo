377310383c376260e2090a6f0837c4b257e16f1f
Michael Gingras <mcg79@cornell.edu>
2023-03-29 19:31:18 -0600

    add logging to qstash endpoint to help with debugging

--- pages/api/qstash.ts
+++ pages/api/qstash.ts
@@ -5,12 +5,16 @@ import { TransactionReceiptNotFoundError, TransactionReceipt } from "viem";
 import { postData } from "@/lib/fetch";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
   try {
     const receipt = (await publicClient.getTransactionReceipt({
       hash: req.body.hash as `0x${string}`,
     })) as TransactionReceipt;
 
+    console.log(receipt);
+
     const fullEndpoint = `${process.env.API_HOST}${req.body.endpoint}`;
+    console.log(fullEndpoint);
     await postData(fullEndpoint, {
       receipt: {
         status: receipt.status,
@@ -22,6 +26,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   } catch (err) {
     if (err instanceof TransactionReceiptNotFoundError) {
       // 202 seems more appropriate but q-stash only retries if it gets a non 2xx response code
+      console.log("error, probably failed transaction, pending result...");
       return res.json({
         statusCode: 500,
         message: "Transaction not yet confirmed on chain, pending result...",
